f0e4a3704be9a2781f15c60dc26554bcd02891da
Carolina Rosatto <carolrosatto@gmail.com>
2021-08-22 20:57:13 -0300

    crud methods

--- src/main/java/com/carolis/todo/controller/TodoController.java
+++ src/main/java/com/carolis/todo/controller/TodoController.java
@@ -1,13 +1,19 @@
 package com.carolis.todo.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.carolis.todo.entity.Todo;
 import com.carolis.todo.service.TodoService;
@@ -15,7 +21,7 @@ import com.carolis.todo.service.TodoService;
 @RestController
 @RequestMapping(value = "/todos")
 public class TodoController {
-	
+
 	@Autowired
 	TodoService service;
 
@@ -23,24 +29,43 @@ public class TodoController {
 	public ResponseEntity<Todo> findById(@PathVariable Integer id) {
 		Todo obj = service.findById(id);
 		return ResponseEntity.ok(obj);
-		
+
 	}
-	
+
 	@GetMapping(value = "/notFinished")
 	public ResponseEntity<List<Todo>> listNotFinished() {
 		List<Todo> list = service.findAllNotFinished();
 		return ResponseEntity.ok(list);
 	}
-	
+
 	@GetMapping(value = "/finished")
 	public ResponseEntity<List<Todo>> listFinished() {
 		List<Todo> list = service.findAllFinished();
 		return ResponseEntity.ok(list);
 	}
-	
+
 	@GetMapping
 	public ResponseEntity<List<Todo>> listAll() {
 		List<Todo> list = service.findAll();
 		return ResponseEntity.ok(list);
+	}
+
+	@PostMapping
+	public ResponseEntity<Todo> create(@RequestBody Todo obj) {
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Todo> update(@PathVariable Integer id, @RequestBody Todo obj) {
+		Todo newObj = service.update(id, obj);
+		return ResponseEntity.ok().body(newObj);
 	}
 }
